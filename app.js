ec7338a22b9eadb01555a4215e52515c1f4ec468
NorbertUsang <norbertusang@yahoo.com>
2021-05-20 20:02:35 -1200

    initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+
+function convertFahrToCelsius (F) {
+    var result = '';
+    let inputData = typeof (F)
+    if (typeof (F) === 'string' || typeof (F) === 'number' ) {
+    let converted = Number(F);
+    let C = (converted - 32) * 5/9;
+    let celsius = C.toFixed(4);
+    return celsius
+    }
+    if (toString.call(F) === "[object Array]") { 
+
+        let arrayValue = JSON.stringify(F);
+        return result = `${arrayValue} is not a valid number but an Array.`
+    }
+    if (F instanceof Object) {
+        let objectValue = JSON.stringify(F);
+        return finallu = `${objectValue} is not a valid number but an object.`
+    } else {
+        result = `${JSON.stringify(F)} is not a valid but a/an ${inputData}.`
+        return result;
+    }
+}
+
+
+function checkYuGiOh(n) {
+    //create an array with n length using es6
+    //use try/catch to validate n
+
+    try {
+        let arr = [...new Array(Number(n))].map((_value, index) => {
+            //increment index which start from 0 to start from 1 index++:
+            //create constants for all the strings to replace
+            const [_2, _3, _5] = ["yu", "gi", "oh"];
+            //check for all
+            if (index % 2 === 0 && index % 3 === 0 && index % 5 === 0)
+                return _2 + "_" + _3 + "_" + _5;
+            //check for two possible multiples
+            else if (index % 2 === 0 && index % 3 == 0) return _2 + "_" + _3;
+            else if (index % 2 === 0 && index % 5 === 0) return _2 + "_" + _5;
+            else if (index % 3 === 0 && index % 5 === 0) return _3 + "_" + _5;
+            //check for one multiple
+            else if (index % 2 === 0) return _2;
+            else if (index % 3 === 0) return _3;
+            else if (index % 5 === 0) return _5;
+            //return the number
+            return index;
+        });
+        return arr;
+    } catch (err) {
+        return `'${n}' is an invalid parameter`;
+    }
+}
+console.log(checkYuGiOh(15));
